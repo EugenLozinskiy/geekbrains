b94797d2329ab2a4132ffaefbe6cfd260218d204
Evgeniy <lozinskii_evgeni@mail.ru>
2018-11-21 23:22:00 +0300

    указать переменные

---
+++ perem/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int number = 8;
+	String text = "Hello Java";
+	char b = 'B';
+	boolean b1 = true;
+	byte getByte = 0;
+	short number2 = 22;
+
+    }
+}
